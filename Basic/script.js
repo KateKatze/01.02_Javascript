0e49caedcf1a0ad14bb476dbe74679d39ada4da1
KateKatze <dr.katekogler@gmail.com>
2022-02-01 13:51:52 +0100

    DONE :)

--- Basic/script.js
+++ Basic/script.js
@@ -1,29 +1,31 @@
 function submitData() {
-    var fname = document.forms["sum"]["fname"].value;
-    var lname = document.forms["sum"]["lname"].value;
-    var age = document.forms["sum"]["bdate"].value;
-    var prof = document.forms["sum"]["IT"].value;
-    var prof = document.forms["sum"]["Hospitality"].value;
-    var prof = document.forms["sum"]["Medicine"].value;
+    var fname = document.getElementById("fname").value;
+    var lname = document.getElementById("lname").value;
+    var age = document.getElementById("bdate").value;
+    var prof1 = document.getElementById("IT").checked;
+    var prof2 = document.getElementById("Hospitality").checked;
+    var prof3 = document.getElementById("Medicine").checked;
+    console.log(prof1, prof2, prof3);
+    var prof = "";
+    var color = "";
+    switch (true) {
+        case (prof1 == true):
+            prof = "IT";
+            color = "purple";
+            break;
 
-    // var sum = fname + lname + bdate;
+        case (prof2 == true):
+            prof = "Hospitality";
+            color = "yellow";
+            break;
 
+        case (prof3 == true):
+            prof = "Medicine";
+            color = "blue";
+            break;
+    }
     var result = document.getElementById("written");
-    result.innerHTML = `<h3>My name is <span id="fnameCheck">${fname}</span> ${lname}, and i am ${age}, working in <span id="profCheck">${prof}</span></h3>`;
-
-    // var fname = document.forms["sum"]["fname"].value;
-    // var length = fname.replace( /\s/g, '' ).length;
-
-    // switch (true) {
-    //     case (length >= 5):
-    //         result.innerHTML = fname;
-    //         result.style.background = "green";
-    //         break;
-    //     case (length < 5):
-    //         result.innerHTML = fname;
-    //         result.style.background = "red";
-    //         break;
-    // }
+    result.innerHTML = `<h3>My name is <span id="fnameCheck">${fname}</span> ${lname}, and i am ${age}, working in <span id="profCheck" style="background: ${color}">${prof}</span></h3>`;
 
     if (fname.length >= 5) {
         document.getElementById("fnameCheck").style.background = "green";
@@ -31,22 +33,7 @@ function submitData() {
         document.getElementById("fnameCheck").style.background = "red";
     }
 
-    var col = document.getElementById("profCheck");
-
-    if (prof = "IT") {
-        col.innerHTML = "IT";
-        col.style.background = "purple";
+    for (let i = 0; i < 6; i++) {
+        document.getElementsByClassName("zero")[i].value = "";
     }
-    if (prof = 'Hospitality') {
-        col.innerHTML = "Hospitality";
-        col.style.background = "yellow";
-    }
-    if (prof = 'Medicine') {
-        col.innerHTML = "Medicine";
-        col.style.background = "blue";
-    } else {
-        col.style.background = "green";
-    }
-
-    return false;
 }
