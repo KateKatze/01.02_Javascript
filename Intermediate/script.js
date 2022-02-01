12534361f153b39bb21341b9a894d50bba6a9049
KateKatze <dr.katekogler@gmail.com>
2022-02-01 15:44:16 +0100

    intermediate done

--- Intermediate/script.js
+++ Intermediate/script.js
@@ -1,14 +1,24 @@
 let santa = "not visible";
-var element = document.getElementsByClassName("pic")
+var element = document.getElementsByClassName("pic");
+console.log(element);
 
-function santaToggle() {
-    if (santa == "not visible") {
-        pic.style.marginLeft = "0px";
-        pic.style.transition = "1s";
-    } else {
-        pic.style.marginLeft = "-400px";
-        santa = "not visible";
-    }
+for (let i = 0; i < element.length; i++) {
+    element[i].addEventListener("click", function() {
+        this.remove();
+    })
 }
 
-document.getElementById("menu-btn").addEventListener("click", santaToggle);
+var pic = document.getElementsByClassName("pic");
+
+function santaToggle() {
+    for (let i = )
+        if (santa == "not visible") {
+            pic.style.marginLeft = "0px";
+            pic.style.transition = "1s";
+        } else {
+            pic.style.marginLeft = "-400px";
+            santa = "not visible";
+        }
+}
+
+document.getElementsByClassName("pic").addEventListener("click", santaToggle);
