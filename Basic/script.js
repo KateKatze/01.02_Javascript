2c0c2b1ba3723f4f6bf420c3e90f9290718941de
KateKatze <dr.katekogler@gmail.com>
2022-02-01 12:40:23 +0100

    ex.2 done

--- Basic/script.js
+++ Basic/script.js
@@ -2,11 +2,14 @@ function submitData() {
     var fname = document.forms["sum"]["fname"].value;
     var lname = document.forms["sum"]["lname"].value;
     var age = document.forms["sum"]["bdate"].value;
+    var prof = document.forms["sum"]["IT"].value;
+    var prof = document.forms["sum"]["Hospitality"].value;
+    var prof = document.forms["sum"]["Medicine"].value;
 
     // var sum = fname + lname + bdate;
 
     var result = document.getElementById("written");
-    result.innerHTML = `<h3>My name is <span id="fnameCheck">${fname}</span> ${lname}, and i am ${age}</h3>`;
+    result.innerHTML = `<h3>My name is <span id="fnameCheck">${fname}</span> ${lname}, and i am ${age}, working in <span id="profCheck">${prof}</span></h3>`;
 
     // var fname = document.forms["sum"]["fname"].value;
     // var length = fname.replace( /\s/g, '' ).length;
@@ -26,6 +29,23 @@ function submitData() {
         document.getElementById("fnameCheck").style.background = "green";
     } else {
         document.getElementById("fnameCheck").style.background = "red";
+    }
+
+    var col = document.getElementById("profCheck");
+
+    if (prof = "IT") {
+        col.innerHTML = "IT";
+        col.style.background = "purple";
+    }
+    if (prof = 'Hospitality') {
+        col.innerHTML = "Hospitality";
+        col.style.background = "yellow";
+    }
+    if (prof = 'Medicine') {
+        col.innerHTML = "Medicine";
+        col.style.background = "blue";
+    } else {
+        col.style.background = "green";
     }
 
     return false;
